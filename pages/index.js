d9902a255099552afcc9b97bdd690698f0daff5d
Vishal-Ravan <vksingh2992000@gmail.com>
2023-06-09 18:07:15 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,102 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import logo from "../images/logo.png";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="container flex min-w-full bg-zinc-100">
+        <div className="w-1/1 ">
+          <Image src={logo} width={190} alt="" />
+          <div className="flex justify-center p-20">
+            <div className="p-10 bg-white border-gray-300 border-solid w-90 rounded-2xl border-1">
+              <h4 className="mt-0 mb-5 text-6xl font-extrabold text-left text-gray-900 leading-20 ">
+                Make Your Brand a next <br /> social media star &#x2B50;
+              </h4>
+              <p className="pt-5 text-base ">
+                Sets the width of the element to a specific width of 64 pixels.
+                Additionally, you can combine the w-1/2 class with other utility
+                classes or use responsive variants to apply different widths at
+                different screen
+              </p>
+              <p className="pt-4 text-base">
+                different widths at different screen sizes. For example,
+                md:w-2/3 sets the width of the element
+              </p>
+              <div className="flex justify-between pt-5 mt-10 align-middle ">
+                <button className="flex pt-5 pb-5 font-bold tracking-wider text-white rounded-full p-36 bg-cyan-600">
+                  Get Started{" "}
+                  <svg
+                    data-te-animation-init
+                    data-te-animation-reset="true"
+                    data-te-animation="[slide-right_1s_ease-in-out]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-7 w-8 pl-3"
+                  >
+                    <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
+                  </svg>
+                </button>
+                <h6 className="flex items-center justify-center text-base font-bold leading-9 text-cyan-600">
+                  How it's Works
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-full w-1/1">
+          <div className="w-1/2">
+            <logo-slider>
+              <div>
+                <img src="https://www.worldphoto.org/sites/default/files/%C2%A9%20Henry%20Oude%20Egberink%2C%20Netherlands%2C%20entry%2C%20Open%20competition%20%2C%20Creative%20%2C%202020%20Sony%20World%20Photography%20Awards.jpg" />
+                <img src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" />
+                <img src="https://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Alone-Boy-Images.jpg" />
+                <img src="https://images.pexels.com/photos/2816893/pexels-photo-2816893.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+                <img src="https://images.pexels.com/photos/1510149/pexels-photo-1510149.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+                <img src="https://images.pexels.com/photos/1510149/pexels-photo-1510149.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+                <img src="https://i.pinimg.com/222x/a9/67/91/a96791a3c302f73450f8db4c39bb0a5b.jpg" />
+                <img src="https://cdn.shopify.com/s/files/1/0603/6412/8462/files/cute-good-morning-pics-for-2023_13_600x600.jpg?v=1672142598" />
+              </div>
+
+              <div>
+                <img src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-1662654376.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=1200:*" />
+                <img src="https://cdn.shopify.com/s/files/1/0603/6412/8462/files/cute-good-morning-pics-for-2023_13_600x600.jpg?v=1672142598" />
+                <img src="https://i.pinimg.com/736x/0e/70/b0/0e70b0f6712f3b8e9d04ca18d902ef83.jpg" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoiHSEmqep8irLEF_qh3jKZFw3hIYrPNkpg7uakggISOyzlzuuGGjinq_7y4oboMAmFo2jLLKSbk&usqp=CAU&ec=48600113" />
+                <img src="https://photosfile.com/wp-content/uploads/2022/04/Funny-DP-29.jpeg" />
+                <img src="https://static.boredpanda.com/blog/wp-content/uploads/2020/01/funny-celebrities-social-media-memes-14-5e2b0580712b1__700.jpg" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoiHSEmqep8irLEF_qh3jKZFw3hIYrPNkpg7uakggISOyzlzuuGGjinq_7y4oboMAmFo2jLLKSbk&usqp=CAU&ec=48600113" />
+                <img src="https://www.whatsappimages.in/wp-content/uploads/2021/12/Free-HD-Funny-Whatsapp-DP-Pics-Download.jpg" />
+              </div>
+            </logo-slider>
+          </div>
+          <div className="w-1/2">
+            <logo-slider className="lslider">
+              <div>
+                <img src="https://picsum.photos/100" />
+                <img src="https://picsum.photos/102" />
+                <img src="https://picsum.photos/103" />
+                <img src="https://picsum.photos/104" />
+                <img src="https://picsum.photos/105" />
+                <img src="https://picsum.photos/106" />
+                <img src="https://picsum.photos/107" />
+                <img src="https://picsum.photos/108" />
+              </div>
+
+              <div>
+                <img src="https://picsum.photos/100" />
+                <img src="https://picsum.photos/102" />
+                <img src="https://picsum.photos/103" />
+                <img src="https://picsum.photos/104" />
+                <img src="https://picsum.photos/105" />
+                <img src="https://picsum.photos/106" />
+                <img src="https://picsum.photos/107" />
+                <img src="https://picsum.photos/108" />
+              </div>
+            </logo-slider>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
