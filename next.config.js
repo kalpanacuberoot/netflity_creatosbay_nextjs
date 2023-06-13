9f670b5920e166d9a3a480d4b3087637ea69459f
Vishal-Ravan <vksingh2992000@gmail.com>
2023-06-13 13:48:52 +0530

    create responsive all pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'dist',
+  experimental:{
+    images:{
+      domains:["images.unsplash.com"],
+      unoptimized: true,
+    },
+  }
+  }
+ 
+
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,7 @@ export default function Home() {
             {currentSection === 1 && (
               <div className="p-4 lg:px-10 bg-white border-gray-300 border-solid w-full  rounded-2xl border-1">
                 <h4 className="mt-0 mb-5 font-extrabold text-left text-gray-900 leading-20 text-base sm:text-lg md:text2xl lg:text-3xl xl:text-5xl ">
-                  Make Your Brand a next
-                  social media star &#x2B50;
+                  Make Your Brand a next social media star &#x2B50;
                 </h4>
                 <p className="pt-5  text-base sm:text-lg md:text-lg lg:text-xl xl:text-xl ">
                   Sets the width of the element to a specific width of 64
@@ -57,7 +56,7 @@ export default function Home() {
                     </svg>
                   </button>
                   <h6 className="flex items-center w-full justify-center text-base font-bold leading-9 text-cyan-600">
-                    How it's Works 
+                    How it&#39;s Works
                   </h6>
                 </div>
               </div>
@@ -189,7 +188,7 @@ export default function Home() {
                     type="submit"
                     onClick={handleGetStarted}
                     className="bg-cyan-600 hover:bg-cyan-600 rounded-3xl mt-5 text-white w-full  font-bold py-3 px-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl focus:outline-none focus:shadow-outline"
-                    >
+                  >
                     Next
                   </button>
                 </form>
@@ -246,50 +245,209 @@ export default function Home() {
           <div className="w-1/2">
             <logo-slider>
               <div>
-                <img src="https://www.worldphoto.org/sites/default/files/%C2%A9%20Henry%20Oude%20Egberink%2C%20Netherlands%2C%20entry%2C%20Open%20competition%20%2C%20Creative%20%2C%202020%20Sony%20World%20Photography%20Awards.jpg" />
-                <img src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" />
-                <img src="https://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Alone-Boy-Images.jpg" />
-                <img src="https://images.pexels.com/photos/2816893/pexels-photo-2816893.jpeg?auto=compress&cs=tinysrgb&w=1600" />
-                <img src="https://images.pexels.com/photos/1510149/pexels-photo-1510149.jpeg?auto=compress&cs=tinysrgb&w=1600" />
-                <img src="https://images.pexels.com/photos/1510149/pexels-photo-1510149.jpeg?auto=compress&cs=tinysrgb&w=1600" />
-                <img src="https://i.pinimg.com/222x/a9/67/91/a96791a3c302f73450f8db4c39bb0a5b.jpg" />
-                <img src="https://cdn.shopify.com/s/files/1/0603/6412/8462/files/cute-good-morning-pics-for-2023_13_600x600.jpg?v=1672142598" />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686317278186-be66e4db684e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
               </div>
 
               <div>
-                <img src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-1662654376.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=1200:*" />
-                <img src="https://cdn.shopify.com/s/files/1/0603/6412/8462/files/cute-good-morning-pics-for-2023_13_600x600.jpg?v=1672142598" />
-                <img src="https://i.pinimg.com/736x/0e/70/b0/0e70b0f6712f3b8e9d04ca18d902ef83.jpg" />
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoiHSEmqep8irLEF_qh3jKZFw3hIYrPNkpg7uakggISOyzlzuuGGjinq_7y4oboMAmFo2jLLKSbk&usqp=CAU&ec=48600113" />
-                <img src="https://photosfile.com/wp-content/uploads/2022/04/Funny-DP-29.jpeg" />
-                <img src="https://static.boredpanda.com/blog/wp-content/uploads/2020/01/funny-celebrities-social-media-memes-14-5e2b0580712b1__700.jpg" />
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQoiHSEmqep8irLEF_qh3jKZFw3hIYrPNkpg7uakggISOyzlzuuGGjinq_7y4oboMAmFo2jLLKSbk&usqp=CAU&ec=48600113" />
-                <img src="https://www.whatsappimages.in/wp-content/uploads/2021/12/Free-HD-Funny-Whatsapp-DP-Pics-Download.jpg" />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
               </div>
             </logo-slider>
           </div>
           <div className="w-1/2">
             <logo-slider className="lslider">
               <div>
-                <img src="https://picsum.photos/100" />
-                <img src="https://picsum.photos/102" />
-                <img src="https://picsum.photos/103" />
-                <img src="https://picsum.photos/104" />
-                <img src="https://picsum.photos/105" />
-                <img src="https://picsum.photos/106" />
-                <img src="https://picsum.photos/107" />
-                <img src="https://picsum.photos/108" />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
               </div>
-
               <div>
-                <img src="https://picsum.photos/100" />
-                <img src="https://picsum.photos/102" />
-                <img src="https://picsum.photos/103" />
-                <img src="https://picsum.photos/104" />
-                <img src="https://picsum.photos/105" />
-                <img src="https://picsum.photos/106" />
-                <img src="https://picsum.photos/107" />
-                <img src="https://picsum.photos/108" />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
+                <Image
+                  src="https://images.unsplash.com/photo-1686538653516-7e2c9ced00aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                  alt=""
+                  width={450}
+                  height={100}
+                />
               </div>
             </logo-slider>
           </div>
