b6cbd6b90267d34cd0d26e392ba5a45f2ca611c3
kalpanacuberoot <kalpana@cuberoot.tech>
2023-08-26 21:08:05 +0530

    feat: added change password api in change password page

--- components/Homepage/Modal_change_password/Change_password_content/index.js
+++ components/Homepage/Modal_change_password/Change_password_content/index.js
@@ -2,16 +2,21 @@ import Colors from "@/styles/Colors";
 import ModalHeader from "../../ModalHeader";
 import { useState } from "react";
 import Buttons from "@/components/Button";
+import { apiCall, url } from "@/generalfunation";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import Cookies from "js-cookie";
 
 const Change_password_content = () => {
 
 
-    const [currentpassword,setCurrentpassword] = useState('');
+    const [currentpassword, setCurrentpassword] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [passwordsMatch, setPasswordsMatch] = useState(true);
 
-    
+
     const handlePasswordChange = (e) => {
         const newPassword = e.target.value;
         setPassword(newPassword);
@@ -28,6 +33,63 @@ const Change_password_content = () => {
         setPasswordsMatch(password === newConfirmPassword);
     };
 
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        const cookieValue = JSON.parse(Cookies.get('user_data'));
+        console.log('categories cookieValue------------1', cookieValue?.token);
+
+        try {
+            const postData = {
+                "old_password": "test1234",
+                "new_password": "test123456"
+            };
+
+            const headers = {
+                'Authorization': `Bearer ${cookieValue?.token}`,
+                'Content-Type': 'application/json',
+            };
+
+            const response = await fetch(`${url}/changepassword`, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(postData),
+            });
+            console.log('changepassword response:', response);
+
+            if (response?.ok) {
+                const responseData = await response.json();
+                console.log('changepassword response:2', responseData);
+
+                if (responseData.status) {
+                    toast.success('changepassword response: Successfully Created', {
+                        position: 'top-center',
+                        autoClose: 5000,
+                    });
+
+                    // router.push('/home');
+                } else {
+                    console.error('changepassword Error:', responseData.message);
+                    // alert('Brand creation failed');
+                }
+            } else {
+                if(response?.status === 401){
+                    console.error('changepassword---Error:', response?.statusText);
+                // alert('Brand creation failed');
+
+                toast.error('Invalid old password', {
+                    position: 'top-center',
+                    autoClose: 5000,
+                });
+                }
+                
+            }
+        } catch (error) {
+            console.error('changepassword changepassword Error:', error);
+        }
+    };
+
     return (
         <>
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -38,16 +100,17 @@ const Change_password_content = () => {
                         <h1 className="font-bold line-height-[26.52px] mb-5">
                             Change Password
                         </h1>
-                        <form>
+                        <form onSubmit={handleSubmit}>
 
                             <div className="flex flex-col">
                                 <input
-                                    type="email"
-                                    id="name"
+                                    type="password"
+
                                     className="appearance-none border rounded-md w-full py-5 bg-gray-100 px-3 mr-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     placeholder="Current Password"
                                     value={currentpassword}
-                                    onChange={() => setCurrentpassword(e.target.value)}
+                                    onChange={(e) => setCurrentpassword(e.target.value)}
+                                    required
                                 />
                                 <input
                                     type="password"
