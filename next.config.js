1e51cf0d794f0cb55ff570de467f9915706aa820
kalpanacuberoot <kalpana@cuberoot.tech>
2023-09-12 18:04:13 +0530

    fix: payumoney is integrating in creator_amountpage and solved env process

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    // domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com", 't4.ftcdn.net', 'imgs.search.brave.com', 'creatorsbay-media-bucket.s3.ap-south-1.amazonaws.com', '8b5a-2405-201-402a-d04f-6138-ee21-1aa3-6890.ngrok-free.app'], // Add the domain(s) you need here
+  },
+  env:{
 
-    domains: ["images.unsplash.com",'t4.ftcdn.net','imgs.search.brave.com', 'creatorsbay-media-bucket.s3.ap-south-1.amazonaws.com','8b5a-2405-201-402a-d04f-6138-ee21-1aa3-6890.ngrok-free.app'], // Add the domain(s) you need here
-    // domains: ['8b5a-2405-201-402a-d04f-6138-ee21-1aa3-6890.ngrok-free.app'],
-  },
-  env: {
-    API_URL: 'https://backend.creatorsbay.app/api',
-  },
-  
+  }
+ 
 }
-
-
 
 module.exports = nextConfig
